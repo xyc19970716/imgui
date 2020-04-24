f2ebfb4fc038bdce01742f1d750d5ae2ea0e1744
Ben Carter <ben@shironekolabs.com>
2020-04-24 19:30:20 +0200

    Shadows, Backends: Moved shadow texture baking configuration to style, added font atlas API to request reloads. (experimental)

--- imgui_internal.h
+++ imgui_internal.h
@@ -116,7 +116,6 @@ struct ImGuiPopupData;              // Storage for current popup stack
 struct ImGuiSettingsHandler;        // Storage for one type registered in the .ini file
 struct ImGuiStackSizes;             // Storage of stack sizes for debugging/asserting
 struct ImGuiStyleMod;               // Stacked style modifier, backup of modified data so we can restore it
-struct ImGuiStyleShadowTexConfig;   // Shadow Texture baking config
 struct ImGuiTabBar;                 // Storage for a tab bar
 struct ImGuiTabItem;                // Storage for a tab item (within a tab bar)
 struct ImGuiTable;                  // Storage for a table
@@ -1512,6 +1511,7 @@ struct ImGuiContext
     int                     WantCaptureKeyboardNextFrame;
     int                     WantTextInputNextFrame;
     char                    TempBuffer[1024 * 3 + 1];           // Temporary text buffer
+    bool                    WantStyleUpdateTextureInEndFrame;   // Do we want to do StyleUpdateTexture() at the end of the frame?
 
     ImGuiContext(ImFontAtlas* shared_font_atlas)
     {
@@ -1657,6 +1657,7 @@ struct ImGuiContext
         FramerateSecPerFrameAccum = 0.0f;
         WantCaptureMouseNextFrame = WantCaptureKeyboardNextFrame = WantTextInputNextFrame = -1;
         memset(TempBuffer, 0, sizeof(TempBuffer));
+        WantStyleUpdateTextureInEndFrame = false;
     }
 };
 
@@ -2520,7 +2521,6 @@ namespace ImGui
     IMGUI_API void          DebugRenderViewportThumbnail(ImDrawList* draw_list, ImGuiViewportP* viewport, const ImRect& bb);
 
 } // namespace ImGui
-
 
 //-----------------------------------------------------------------------------
 // [SECTION] ImFontAtlas internal API
